7cbf528edcfe7809bc9bf2fa292b3cae7dce4f2e
ucha19871 <ucha19871@gmail.com>
2017-06-01 18:14:07 +0400

    test token remove

--- tests/FBTest.php
+++ tests/FBTest.php
@@ -22,8 +22,8 @@ class FBTest extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        $app['config']->set('services.firebase.database_url', 'https://copart-f335a.firebaseio.com');
-        $app['config']->set('services.firebase.secret', 'KB2xZjJgAvmVyB1hWr0RT8ykNrT6f2emuuaxJTr9');
+        $app['config']->set('services.firebase.database_url', 'https://PROJECT.firebaseio.com');
+        $app['config']->set('services.firebase.secret', 'KB2xZjJgAvmVyPROJECT6f2emuuaxJTr9');
     }
 
     public function testSetFunction()
